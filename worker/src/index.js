bffd6daa7ac64ef17932d3145bacefee9904d684
amirsepehr2020 <sepehr2sodoury@gmail.com>
2026-07-14 20:37:09 +0330

    Update index.js

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,1 +1,35 @@
+export default {
 
+    async fetch(request) {
+
+        return new Response(
+
+            JSON.stringify({
+
+                success: true,
+
+                message: "Spix Worker Online 🚀"
+
+            }),
+
+            {
+
+                headers: {
+
+                    "Content-Type": "application/json",
+
+                    "Access-Control-Allow-Origin": "*",
+
+                    "Access-Control-Allow-Headers": "*",
+
+                    "Access-Control-Allow-Methods": "*"
+
+                }
+
+            }
+
+        );
+
+    }
+
+}
